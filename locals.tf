d94198e3e62f47002fc769c42672db67b076b7c4
Shmuel Raichman <22668282+shmuel-raichman@users.noreply.github.com>
2023-05-15 23:27:18 +0300

    Added india fix

--- locals.tf
+++ locals.tf
@@ -4,5 +4,6 @@ locals {
     us-east-1      = "ami-037ff6453f0855c46"
     eu-central-1   = "ami-0764964fdfe99bc31"
     ap-northeast-1 = "ami-04f47c2ec43830d77"
+    ap-south-1 = "ami-0fd505d7f13496375"
   }
 }
