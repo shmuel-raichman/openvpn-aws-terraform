ac9b55cbac287c7b1cafe60ffaa9049cde65f347
Shmuel Raichman <22668282+shmuel-raichman@users.noreply.github.com>
2024-11-24 18:54:08 +0200

    Update locals.tf
    Added israel ami

--- locals.tf
+++ locals.tf
@@ -1,11 +1,12 @@
 # B"H
 locals {
   images = {
-    us-east-1      = "ami-037ff6453f0855c46"
-    eu-central-1   = "ami-0764964fdfe99bc31"
-    ap-northeast-1 = "ami-04f47c2ec43830d77"
-    ap-south-1 = "ami-0fd505d7f13496375"
-    af-south-1 = "ami-0befbf2ca69d90686"
-    ap-southeast-1 = "ami-07a19a0c938d5f577"
+    us-east-1        = "ami-037ff6453f0855c46"
+    eu-central-1     = "ami-0764964fdfe99bc31"
+    ap-northeast-1   = "ami-04f47c2ec43830d77"
+    ap-south-1       = "ami-0fd505d7f13496375"
+    af-south-1       = "ami-0befbf2ca69d90686"
+    ap-southeast-1   = "ami-07a19a0c938d5f577"
+    il-central-1     = "ami-0ee9254cfcec3ad63"
   }
 }
